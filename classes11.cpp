3dd3ac4d4b474c1ba86c501d2bfad53fb6508f0a
eddybruv <edwinajong@gmail.com>
2021-07-21 20:36:07 +0100

    derived classes

---
+++ classes11.cpp
@@ -0,0 +1,43 @@
+/*Date: 20-07-2021
+    by eddybruv
+    learning classes*/
+
+
+
+//Derived classes
+#include<iostream>
+using namespace std;
+
+class CPolygon{
+    protected:
+        int width, height;
+    public:
+        void set_values(int a, int b){
+            width = a;
+            height = b;
+        }
+};
+
+class CRectangle: public CPolygon{
+    public:
+        int area(){
+            return ( width*height);
+        }
+};
+
+class CTriangle: public CPolygon{
+    public:
+        int area(){
+            return (width * height/2);
+        }
+};
+
+int main(){
+    CRectangle rect; 
+    CTriangle trgl;
+    rect.set_values (2,3);
+    trgl.set_values (4,3);
+    cout << rect.area() << endl;
+    cout << trgl.area() << endl;
+    return 0;
+}
